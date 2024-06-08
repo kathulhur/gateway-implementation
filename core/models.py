e5bff31e2c767a02855366da7829853a66bba773
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2024-06-08 18:05:56 +0800

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import uuid
+
+class InferenceServiceMapping(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    service_name = models.CharField(max_length=255, unique=True)
+    ip_address = models.GenericIPAddressField()
+    port = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.service_name} -> {self.ip_address}:{self.port}"
