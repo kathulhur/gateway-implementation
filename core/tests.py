ec15086fc90b08c1eba171e112b58320e0f88145
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2024-06-08 18:32:22 +0800

    Add placeholder test for each endpoint

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class InformationEndpointTest(APITestCase):
+     def test_information_endpoint_available(self):
+        response = self.client.get('/api/inference/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class InferenceEndpointTest(APITestCase):
+
+    def test_information_endpoint_available(self):
+        response = self.client.get('/api/info/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
